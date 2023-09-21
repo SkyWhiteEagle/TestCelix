7a4561b330679d14d09d10bd1201f51b2caf2e79
WhiteEagle <skywhite.eagle@laposte.net>
2023-09-21 18:41:43 +0200

    Dump bundles

--- doTheThing-debug.sh
+++ doTheThing-debug.sh
@@ -1,7 +1,7 @@
 mkdir build
 cd build
 conan install .. --build missing -pr debug
-conan build ..
+conan build .. -pr debug
 cd Debug/deploy/TestCelixContainer/
 source ../../generators/conanrun.sh
 ./TestCelixContainer
--- doTheThing.sh
+++ doTheThing.sh
@@ -1,7 +1,6 @@
 mkdir build
 cd build
-conan install .. --build missing
-conan build ..
+conan build .. --build missing -s:h build_type=Release
 cd Release/deploy/TestCelixContainer/
 source ../../generators/conanrun.sh
 ./TestCelixContainer
